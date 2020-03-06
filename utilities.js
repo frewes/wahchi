0dc26d934476eaca5124ed265a7b4ef0796a5b72
Fred Westling <fredrik.westling@gmail.com>
2020-03-06 15:02:09 +1100

    Finished all the pages!

---
+++ utilities.js
@@ -0,0 +1,23 @@
+const test = e => {
+  console.log(e);
+  console.log(e.getAttribute("for"));
+  const tabLinks = document.getElementsByClassName("tab-link");
+  console.log(tabLinks);
+  Array.from(tabLinks).forEach(tl => {
+    if (tl !== e) tl.classList.remove("selected");
+  });
+  e.classList.add("selected");
+
+  const tabs = document.getElementsByClassName("tab");
+  Array.from(tabs).forEach(t => {
+    console.log(t.getAttribute("id"));
+    if (t.getAttribute("id") !== e.getAttribute("for")) {
+      t.classList.add("tab-closed");
+      t.classList.remove("tab-open");
+      console.log("TEST");
+    } else {
+      t.classList.remove("tab-closed");
+      t.classList.add("tab-open");
+    }
+  });
+};
